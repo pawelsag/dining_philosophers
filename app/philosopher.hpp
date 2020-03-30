a6d47bb0c45d020b3642f690c5090034bb6bd3df
windspring <sagan.pawel1000@gmail.com>
2020-03-30 23:32:46 +0200

    refactoring code

--- app/philosopher.hpp
+++ app/philosopher.hpp
@@ -1,26 +1,41 @@
 #pragma once
 #include <condition_variable>
+#include <atomic>
+#include <mutex>
 namespace so2
 {
-    class philosopher
-    {
-        std::condition_variable cv;
-        std::mutex t_mutex;
+    struct waiter;
 
-    public:
+    struct philosopher
+    {
         void think();
         void try_eat();
-        void notify();
+        bool forks_ready();
+
+        static inline std::mutex t_mutex = {};
+        static inline std::condition_variable cv = {};
+        static inline int next_ph = 0;
+        
+        int curent_ph = next_ph++;
+        std::atomic<bool> forks_accessible;
+        //const waiter& w;
     };
 
     void philosopher::think()
     {
-        //there will be some kind of ncurses view     
+       std::unique_lock<std::mutex> lk(t_mutex);
+       //there will be some kind of ncurses view 
+       forks_accessible = false; 
+       cv.wait(lk, [&]{return forks_accessible.load();});
     }
 
-    void philosopher::notify()
+    bool philosopher::forks_ready()
     {
-             
+       if(forks_accessible == true)
+           return false;
+
+       forks_accessible = true;
+       return true;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <fmt/format.h>
 #include <iostream>       // std::cin, std::cout
 #include <queue>          // std::queue
-#include "waitress.hpp"
+#include "waiter.hpp"
 
 int main ()
 {
     fmt::print("Hello!\n");
-    so2::waitress w_obj;
+    so2::waiter w_obj;
 }
